57ac2fe8035dca8f1852340aa4f8ab089ebc5b27
Victor Häggqvist <victor@snilius.com>
2014-03-04 20:39:58 +0100

    woop

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
-    console.log('inputEntered: ' + text);
+    //console.log('inputEntered: ' + text);
     //alert('Make QR of "' + text + '"');
     chrome.tabs.create({
         'url': chrome.extension.getURL('qr.html#'+text)
